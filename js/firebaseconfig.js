d5a242182d5d1b1faf00d07e7b3e8d81487a3e5f
Filemon Sang <filemonsang@gmail.com>
2026-08-23 17:24:08 -0400

    home page html/css v1 complete, firebase downloaded

---
+++ js/firebaseconfig.js
@@ -0,0 +1,19 @@
+import { getFirestore} from 'firebase/firestore'
+import { initializeApp } from 'firebase/app'
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpei56oya7L0CfICnOTMA0u4VjRtR6KfU",
+  authDomain: "southportkeyclubwebsite.firebaseapp.com",
+  projectId: "southportkeyclubwebsite",
+  storageBucket: "southportkeyclubwebsite.firebasestorage.app",
+  messagingSenderId: "128387313566",
+  appId: "1:128387313566:web:8af6fc61aef1d463b2b08f",
+  measurementId: "G-LH06BZ5T6Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+const auth = getAuth(app);
+export {auth, database};
+
